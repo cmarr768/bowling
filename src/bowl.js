23207ef560dcf5475111d15d2d0c84e69c20c29b
Colin Marr <colinm@ifit.com>
2021-06-17 11:45:19 -0400

    parse first and second roll of frame

--- src/bowl.js
+++ src/bowl.js
@@ -1,5 +1,14 @@
-const run = () => {
-  return;
+const run = (frames) => {
+  let totalScore = 0;
+  frames.forEach((frame) => {
+    const first = frame.charAt(0);
+    const second = frame.length > 1 ? frame.charAt(1) : 0;
+    console.log(first, second);
+    const score = parseInt(first) + parseInt(second);
+    console.log(score);
+    totalScore += score;
+  });
+  return totalScore;
 };
 
 module.exports = {
--- src/bowl.test.js
+++ src/bowl.test.js
@@ -4,4 +4,12 @@ describe('bowl', () => {
   it('should be a function', () => {
     expect(typeof bowl.run === 'function').toBeTruthy();
   });
+
+  describe('when all frames have number scores', () => {
+    it('should return the total scrore of the frames passed in', () => {
+      const frames = ['11', '22', '33'];
+      const response = bowl.run(frames);
+      expect(response).toBe(12);
+    });
+  });
 });
