e8be8963562e5d00c3362d4f1adf21c752facb23
Colin Marr <colinm@ifit.com>
2021-06-29 10:41:51 -0400

    small refactor

--- src/bowl.js
+++ src/bowl.js
@@ -15,24 +15,7 @@ const run = (frames) => {
   let game = {};
   const allFrames = frames.split(' ');
   allFrames.forEach((frame, index) => {
-    const rolls = frame.split('');
-    const first = rolls[0];
-    const firstRoll = getRoll(first);
-    const second = rolls.length > 1 ? rolls[1] : undefined;
-    const secondRoll = getRoll(second);
-    const third = frames.length > 2 ? rolls[2] : undefined;
-    const thirdRoll = getRoll(third);
-    const score = firstRoll + secondRoll + thirdRoll;
-
-    game[index + 1] = {
-      frame: index + 1,
-      firstRoll,
-      secondRoll,
-      thirdRoll,
-      score,
-      isStrike: isStrike(frame),
-      isSpare: isSpare(frame),
-    };
+    game[index + 1] = getFrame(frame, index);
   });
 
   console.log('game', Object.values(game));
@@ -55,6 +38,27 @@ const run = (frames) => {
   return newTotalScore;
 };
 
+const getFrame = (frame, index) => {
+  const rolls = frame.split('');
+  const first = rolls[0];
+  const firstRoll = getRoll(first);
+  const second = rolls.length > 1 ? rolls[1] : undefined;
+  const secondRoll = getRoll(second);
+  const third = frames.length > 2 ? rolls[2] : undefined;
+  const thirdRoll = getRoll(third);
+  const score = firstRoll + secondRoll + thirdRoll;
+
+  return {
+    frame: index + 1,
+    firstRoll,
+    secondRoll,
+    thirdRoll,
+    score,
+    isStrike: isStrike(frame),
+    isSpare: isSpare(frame),
+  };
+};
+
 const getRoll = (roll) => {
   if (!roll) return 0;
   switch (roll.toLowerCase()) {
@@ -75,12 +79,6 @@ const isStrike = (frame) => {
 
 const isSpare = (frame) => {
   return frame.toLowerCase().indexOf('/') > -1;
-};
-
-const getFrameScore = (firstRoll, secondRoll) => {
-  const firstRollScore = firstRoll === '-' ? 0 : parseInt(firstRoll);
-  const secondRollScore = secondRoll === '-' ? 0 : parseInt(secondRoll);
-  return firstRollScore + secondRollScore;
 };
 
 module.exports = {
